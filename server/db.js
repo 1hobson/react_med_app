a306bae1800faee4bba6a8c68f5266abd04b251a
1hobson <j.hobs@hotmail.co.uk>
2025-09-17 10:29:00 -0400

    third commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:tMCvNhHL1kEEFiVu3mijwN25@172.21.91.187:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
