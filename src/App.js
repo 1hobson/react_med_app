29ffcf611df003213a2c972653cae7adc98f4c26
1hobson <j.hobs@hotmail.co.uk>
2025-09-24 10:06:14 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import SignUp from './Components/SignUp/SignUp';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';  
 import Appointments from './Components/Appointments/Appointments';  
-
+import Notification from './Components/Appointments/Notification';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Notification>
         <Navbar />
-
-        <Routes>
-          <Route path="/" element={<LandingPage />} />            
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/appointments" element={<Appointments />} />
-          <Route path="/instant-consultation" element={<InstantConsultation />} />
-        </Routes>
-        
+            <Routes>
+            <Route path="/" element={<LandingPage />} />            
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/appointments" element={<Appointments />} />
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
+            </Routes>
+        </Notification>
       </BrowserRouter>
     </div>
   );
